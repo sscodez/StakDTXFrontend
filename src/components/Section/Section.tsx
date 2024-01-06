7f7617920be3350b73387e8835e9c5a961466cfd
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2024-01-06 12:20:51 +0500

    Added

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,12 +1,51 @@
 "use client";
-
+import { useContractWrite, useAccount } from "wagmi";
+import { useState } from "react";
+import ERC20TokenFactoryABI from "../../chain-info/abi/ERC20TokenFactoryabi.json";
+import ERC20TokenFactoryAddress from "../../chain-info/contract-address/ERC20TokenFactoryaddress.json";
+import ERC20BurnFactoryABI from "../../chain-info/abi/ERC20BurnFactoryabi.json";
 import React from "react";
 import Button from "../Button/Button";
-import { useState } from "react";
 import { useWeb3Modal } from "@web3modal/wagmi/react";
 const Section = () => {
   const [option, setOption] = useState("liquidity");
   const { open } = useWeb3Modal();
+  const { connector: activeConnector, isConnected, address } = useAccount();
+  const [tokenName, setTokenName] = useState("");
+  const [initalSupply, setInitalSupply] = useState(0);
+  const [burnAmount, setBurnAmount] = useState(0);
+  const [burnToken, setBurnToken] = useState("");
+
+  const { data: useContractWriteData, write: myfunc } = useContractWrite({
+    address: "0x2487FD2FaE2A44DB20488d2BfbB5588AAe26D235",
+    abi: ERC20TokenFactoryABI.abi,
+    functionName: "createToken",
+    args: ["Sa", "Sa", 100000000, "0x4536b462d0ca9449cDfd5C3E766fd0bd6976db5B"],
+    chainId: 97,
+    onError(error: any) {
+      console.log("Error", error);
+      console.log(address);
+    },
+    onSuccess(data) {
+      console.log("Success", data);
+    },
+  });
+
+  const { data: useContractWriteData1, write: myfunc1 } = useContractWrite({
+    address: "0x2487FD2FaE2A44DB20488d2BfbB5588AAe26D235",
+    abi: ERC20TokenFactoryABI.abi,
+    functionName: "burn",
+    args: [burnToken, burnAmount],
+    chainId: 97,
+  });
+
+  const { data: useContractWriteData2, write: myfunc2 } = useContractWrite({
+    address: "0xb6A68FA741e82F86f7EfA0AA95FA59505929A609",
+    abi: ERC20BurnFactoryABI.abi,
+    functionName: "addInitialLiquidity",
+    chainId: 97,
+  });
+
   return (
     <div className="flex flex-col items-center justify-center w-[500px] h-[350px] bg-[#27262C] rounded-[20px] shadow-md">
       <div className="flex items-center justify-center w-full  p-4">
@@ -42,6 +81,7 @@ const Section = () => {
           <input
             className="flex outline-none w-full bg-transparent text-white"
             placeholder="Enter Token Name"
+            onChange={(e: any) => setTokenName(e.target.value)}
           />
         </div>
       )}
@@ -50,6 +90,8 @@ const Section = () => {
           <input
             className="flex outline-none w-full bg-transparent text-white"
             placeholder="Enter Token Supply"
+            value={initalSupply}
+            onChange={(e: any) => setInitalSupply(e.target.value)}
           />
         </div>
       )}
@@ -66,6 +108,8 @@ const Section = () => {
           <input
             className="flex outline-none w-full bg-transparent text-white"
             placeholder="Enter Token  Address to Burn "
+            value={burnToken}
+            onChange={(e: any) => setBurnToken(e.target.value)}
           />
         </div>
       )}
@@ -74,31 +118,49 @@ const Section = () => {
           <input
             className="flex outline-none w-full bg-transparent text-white"
             placeholder="Enter Token  Amount to Burn "
+            value={burnAmount}
+            onChange={(e: any) => setBurnAmount(e.target.value)}
           />
         </div>
       )}
-      {option === "liquidity" && (
+      {option === "liquidity" && [
+        isConnected && (
+          <button
+            onClick={() => myfunc2?.()}
+            className="flex items-center justify-center w-[95%] rounded-[20px] font-bold shadow-md p-4 my-4 bg-[#1fc7d4]"
+          >
+            Add liquidty
+          </button>
+        ),
+      ]}
+      {option === "token" && [
+        isConnected && (
+          <button
+            // onClick={() => open()}
+            className="flex items-center justify-center w-[95%] rounded-[20px] font-bold shadow-md p-4 my-4 bg-[#1fc7d4]"
+            onClick={() => myfunc?.()}
+          >
+            Create Token
+          </button>
+        ),
+      ]}
+      {isConnected &&
+        option === "burn" && [
+          isConnected && (
+            <button
+              onClick={() => myfunc1?.()}
+              className="flex items-center justify-center w-[95%] rounded-[20px] font-bold shadow-md p-4 my-4 bg-[#1fc7d4]"
+            >
+              Burn Token
+            </button>
+          ),
+        ]}
+      {!isConnected && (
         <button
           onClick={() => open()}
           className="flex items-center justify-center w-[95%] rounded-[20px] font-bold shadow-md p-4 my-4 bg-[#1fc7d4]"
         >
-          Add liquidty
-        </button>
-      )}
-      {option === "token" && (
-        <button
-          onClick={() => open()}
-          className="flex items-center justify-center w-[95%] rounded-[20px] font-bold shadow-md p-4 my-4 bg-[#1fc7d4]"
-        >
-          Create Token
-        </button>
-      )}
-      {option === "burn" && (
-        <button
-          onClick={() => open()}
-          className="flex items-center justify-center w-[95%] rounded-[20px] font-bold shadow-md p-4 my-4 bg-[#1fc7d4]"
-        >
-          Burn Token
+          Connect Wallet
         </button>
       )}
     </div>
